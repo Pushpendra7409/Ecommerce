f3fc652dc98deb0a4817bf86fcdee31f602d59c8
Pushpendra7409 <pushpendra7409@gmail.com>
2024-11-22 20:10:56 +0530

    Adding Product Routes test using Postman

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ import Product from '../models/productModel.js'
 
 //Create Product -- Admin 
 
-export const createProduct = async (req,res,next) => {
+export const createProduct = async (req, res, next) => {
 
     const product = await Product.create(req.body);
     res.status(201).json({
@@ -14,10 +14,90 @@ export const createProduct = async (req,res,next) => {
 }
 
 // Get all products
-export const getAllProducts = async (req, res) => {
+export const getAllProducts = async (req, res, next) => {
     const products = await Product.find();
     res.status(200).json({
         success: true,
         products
     });
+};
+
+//Get Product Details
+
+export const getProductDetails = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found",
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        product,
+    });
+};
+
+//Update Product -- Admin
+export const updateProduct = async (req, res, next) => {
+    try {
+        
+        let product = await Product.findById(req.params.id);
+
+        
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found",
+            });
+        }
+
+        
+        product = await Product.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            {
+                new: true, 
+                runValidators: true, 
+            }
+        );
+
+        res.status(200).json({
+            success: true,
+            product,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+
+// Delete Product -- Admin
+
+export const deleteProduct = async (req, res, next) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Product deleted successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
 };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { getAllProducts, createProduct } from '../controllers/productController.js';
+import { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+
 router.route("/product/new").post(createProduct);
+
+router.route("/product/:id").put(updateProduct);
+
+router.route("/product/:id").delete(deleteProduct);
+
+router.route("/product/:id").get(getProductDetails);
 
 export default router;
